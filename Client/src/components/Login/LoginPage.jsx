bac4fea065c2cd6aa24c4caa1b5e1223873a72af
dima <chocolado4kaa12@gmail.com>
2025-03-31 17:29:37 +0300

    added logout func, also added redux

--- Client/src/components/Login/LoginPage.jsx
+++ Client/src/components/Login/LoginPage.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
 import { Header, Resp, Input, Button } from "../Components";
 import "./LoginPage.scss";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "../../redux/authSlice";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const [formState, setFormState] = useState("login");
   const [formData, setFormData] = useState({
     email: "",
     fullName: "",
     password: "",
   });
+  
+  useEffect(() => {
+    if (isLoggedIn) {
+      setFormState("logout");
+    }
+  }, [isLoggedIn]);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -15,6 +28,7 @@ const LoginPage = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    dispatch(login());
     try {
       const response = await fetch("https://localhost:5000/auth/login", {
         method: "POST",
@@ -26,6 +40,11 @@ const LoginPage = () => {
       });
       const data = await response.json();
       console.log("Login Success:", data);
+
+      if (response.ok) {
+        navigate("/");
+      }
+
     } catch (error) {
       console.error("Login Error:", error);
     }
@@ -45,8 +64,29 @@ const LoginPage = () => {
       });
       const data = await response.json();
       console.log("Registration Success:", data);
+
+      if (response.ok) {
+        setFormState("login");
+      }
     } catch (error) {
       console.error("Registration Error:", error);
+    }
+  };
+
+  const handleLogOut = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("https://localhost:5000/auth/logout", {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        dispatch(logout());
+        navigate("/");
+      } else {
+        console.error("Logout failed");
+      }
+    } catch (error) {
+      console.error("Logout Error:", error);
     }
   };
 
@@ -56,35 +96,85 @@ const LoginPage = () => {
       <section className="login_container">
         <div className="login_wrap">
           <form>
-            <Input
-              name="fullName"
-              value={formData.fullName}
-              onChange={handleChange}
-            >
-              Full Name
-            </Input>
-            <Input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            >
-              Email
-            </Input>
-            <Input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-            >
-              Password
-            </Input>
-            <Button className="form_submit" Onclick={handleSubmit} type="submit">
-              Submit
-            </Button>
-            <Button className="form_submit" Onclick={handleRegister} type="submit">
-              Registration
-            </Button>
+            {formState !== "logout" && (
+              <>
+                {formState === "register" && (
+                  <Input
+                    type="text"
+                    name="fullName"
+                    value={formData.fullName}
+                    onChange={handleChange}
+                  >
+                    Full Name
+                  </Input>
+                )}
+                <Input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                >
+                  Email
+                </Input>
+                <Input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                >
+                  Password
+                </Input>
+              </>
+            )}
+
+            {formState === "login" && (
+              <>
+                <Button
+                  className="form_submit"
+                  Onclick={handleSubmit}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+                <Button
+                  className="form_submit"
+                  Onclick={() => setFormState("register")}
+                >
+                  Go To Registration
+                </Button>
+              </>
+            )}
+
+            {formState === "register" && (
+              <>
+                <Button
+                  className="form_submit"
+                  Onclick={handleRegister}
+                  type="submit"
+                >
+                  Register
+                </Button>
+                <Button
+                  className="form_submit"
+                  Onclick={() => setFormState("login")}
+                >
+                  Back to Login
+                </Button>
+              </>
+            )}
+
+            {formState === "logout" && (
+              <>
+                <p>Are you sure you want to quit?</p>
+                <Button
+                  className="form_submit"
+                  Onclick={handleLogOut}
+                  type="reset"
+                >
+                  Log Out
+                </Button>
+              </>
+            )}
           </form>
         </div>
       </section>
